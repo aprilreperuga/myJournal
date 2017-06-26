65439b2f63c772e8ecde4d1fdf6ca749ae543746
April Reperuga <aprilreperuga@gmail.com>
2017-06-26 19:18:23 +0800

    Added a password reset activity function.

--- app/src/main/java/ph/edu/up/mp/reperuga/myjournal/ResetPasswordActivity.java
+++ app/src/main/java/ph/edu/up/mp/reperuga/myjournal/ResetPasswordActivity.java
@@ -1,8 +1,83 @@
 package ph.edu.up.mp.reperuga.myjournal;
 
-/**
- * Created by ApriL on 26/06/2017.
- */
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-class ResetPasswordActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    private EditText mInputEmail;
+    private Button mResetBtn, mBackBtn;
+    private FirebaseAuth mAuth;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        mInputEmail = (EditText) findViewById(R.id.email);
+        mResetBtn = (Button) findViewById(R.id.btn_reset_password);
+        mBackBtn = (Button) findViewById(R.id.btn_back);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+
+            }
+        });
+
+        mResetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = mInputEmail.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)) {
+
+                    Toast.makeText(getApplication(), "Enter your registered email address", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+
+                mProgressBar.setVisibility(View.VISIBLE);
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                if(task.isSuccessful()) {
+
+                                    Toast.makeText(ResetPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+
+                                } else {
+
+                                    Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+
+                                }
+
+                                mProgressBar.setVisibility(View.GONE);
+
+                            }
+                        });
+
+            }
+        });
+
+    }
 }
